b5caff2b324f1430a74f3a81a8d393041acd9eb4
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-30 02:38:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,13 +37,14 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) {|s, i| puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"}
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
 
   def list_artists
-    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
-      puts "#{i}. #{a.name}"
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) {|a, i|
+      puts "#{i+1}. #{a.song.name}"}
     end
   end
 
