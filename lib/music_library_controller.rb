df8532eadf41b4577cdf98ab29adb143ad9841a1
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-30 02:28:49 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -43,9 +43,7 @@ class MusicLibraryController
   end
 
   def list_artists
-    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
-      puts "#{i}. #{a.name}"
-    end
+
   end
 
   def list_genres
