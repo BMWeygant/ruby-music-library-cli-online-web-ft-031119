79cafa56530ccc1e892850cbdf5ef8dcb2243d7a
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-30 01:54:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
 
   def list_artists
     Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
-      puts "#{index}. #{artist.name}"
+       "#{index}. #{artist.name}"
   end
 end
 
