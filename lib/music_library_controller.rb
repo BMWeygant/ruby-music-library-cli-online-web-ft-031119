69dee25fad451725694c0f0319a4c7508390ad6e
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-30 02:46:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
     end
   end
 
-  def list_artists
+  def list_artists(input)
     Artist.find_by_name(input)
       artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.name} - #{s.genre.name}"
